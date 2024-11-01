659fd06d7fc3b3bcf116ea067bde01f481c15f28
duonanotte <166713796+duonanotte@users.noreply.github.com>
2024-11-01 13:22:54 +0300

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     API_ID: int
     API_HASH: str
 
-    USE_RANDOM_DELAY_IN_RUN: bool = True
+    USE_RANDOM_DELAY_IN_RUN: bool = False
     RANDOM_DELAY_IN_RUN: list[int] = [5, 9930]
    
     REF_LINK: str = "https://t.me/TonOldy_bot/app?startapp=NjQzNDA1ODUyMQ=="
